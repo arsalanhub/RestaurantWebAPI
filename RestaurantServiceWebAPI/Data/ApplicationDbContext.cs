c6d30d46cc515bf2e76616adffd7ad6f91412aa2
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-07-20 16:17:54 +0530

    initial commit

---
+++ RestaurantServiceWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantServiceWebAPI.Models;
+
+namespace RestaurantServiceWebAPI.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Menu> Menus { get; set; }
+
+        public static implicit operator ApplicationDbContext(ApplicationBuilder v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestaurantServiceWebAPI/Models/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantServiceWebAPI.Models
+{
+    public class Menu
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+}
+
+
