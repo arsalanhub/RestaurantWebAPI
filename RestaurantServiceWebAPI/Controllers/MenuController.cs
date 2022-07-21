c7a5b13d4f8e037c24af11c03bd7b3f6e29c6676
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-07-21 08:54:42 +0530

    Menu Controller added

---
+++ RestaurantServiceWebAPI/Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantServiceWebAPI.Data;
+using RestaurantServiceWebAPI.Models;
+
+namespace RestaurantServiceWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        [ActivatorUtilitiesConstructor]
+        public MenuController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Menu>> Get() => await _db.Menus.ToListAsync();
+    }
+}
